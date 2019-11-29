86a0987645e29e39396c397f66a559fa150c2d3f
Clément Hallet <clement@challet.eu>
2019-11-29 01:01:13 +0100

    fix base test

--- src/DevBrow.test.js
+++ src/DevBrow.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import DevBrow from './DevBrow';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<DevBrow />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
