45dc14f0169f8065857d403d560bcc78351e27eb
Clément Hallet <clement@challet.eu>
2019-11-29 01:25:22 +0100

    Updates

---
+++ precache-manifest.54e691f75b345c6b8926477a5618c1aa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc9c3ab46983a74ae6a1999db57d13c",
+    "url": "/DevBrow/index.html"
+  },
+  {
+    "revision": "d8a48c081df35ce7f1c4",
+    "url": "/DevBrow/static/css/2.dc669db0.chunk.css"
+  },
+  {
+    "revision": "0149579822d690215b97",
+    "url": "/DevBrow/static/css/main.f6c739b2.chunk.css"
+  },
+  {
+    "revision": "d8a48c081df35ce7f1c4",
+    "url": "/DevBrow/static/js/2.364da2a9.chunk.js"
+  },
+  {
+    "revision": "0149579822d690215b97",
+    "url": "/DevBrow/static/js/main.d8dbf96a.chunk.js"
+  },
+  {
+    "revision": "f189a39f9dd3325f8533",
+    "url": "/DevBrow/static/js/runtime-main.8032767f.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/DevBrow/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/DevBrow/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/DevBrow/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/DevBrow/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/DevBrow/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/DevBrow/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/DevBrow/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/DevBrow/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/DevBrow/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/DevBrow/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/DevBrow/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/DevBrow/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/DevBrow/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/DevBrow/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/DevBrow/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/DevBrow/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
